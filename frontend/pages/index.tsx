fae7eb3fc3989716731552702be43ca38c57618c
Abdurahman Shalil <abdurahmanshalil@hotmail.com>
2020-12-19 13:01:13 +0100

    Update Logo Usage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => (
   <div className={styles.container}>
     <Head>
       <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/logo.svg" />
     </Head>
 
     <main className={styles.main}>
@@ -48,7 +48,7 @@ const Home: React.FC = () => (
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer">
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        Powered by <img src="/logo.svg" alt="Vercel Logo" className={styles.logo} />
       </a>
     </footer>
   </div>
