6b7214c08a85eaede3ac7e09da8c73c12a30449d
Abdurahman Shalil <abdurahmanshalil@hotmail.com>
2020-12-19 17:21:27 +0100

    Export to meta component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,29 +1,9 @@
-import Head from 'next/head';
-
+import Meta from '../components/Meta';
 import styles from '../scss/Home.module.scss';
 
 const Home: React.FC = () => (
   <div className={styles.container}>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/icon/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/icon/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/icon/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/icon/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
-      <link rel="manifest" href="/icon/manifest.json" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/icon/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" />
-    </Head>
+    <Meta />
 
     <main className={styles.main}>
       <h1 className={styles.title}>Welcome to the Auctioning App</h1>
